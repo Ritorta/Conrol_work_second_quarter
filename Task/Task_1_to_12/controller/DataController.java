e5bc9772cac4bb2364d528ce82eb50526d3993e3
Ritorta <Meddesu@yandex.ru>
2024-02-23 19:19:59 +0500

    Create menu in DataController

--- Task/Task_1_to_12/controller/DataController.java
+++ Task/Task_1_to_12/controller/DataController.java
@@ -2,7 +2,6 @@ package Task.Task_1_to_12.controller;
 
 import java.util.Scanner;
 import java.util.InputMismatchException;
-
 import Task.Task_1_to_12.service.ServiceDatabase;
 
 public class DataController {
@@ -15,14 +14,34 @@ public class DataController {
         scanner = new Scanner(System.in);
     }
 
-
-
     public void consoleMenu() {
-        System.out.println("Menu: ");
-        System.out.println("1. Show all animals in the registry.");
-        System.out.println("2. Add new animal.");
-        System.out.println("3. Add new command to animal.");
-        System.out.println("4. Show list of commands for animal.");
-        System.out.println("5. Exit.");
-}
+        while(true) {
+            try {
+                System.out.println("Menu: ");
+                System.out.println("1. Show all animals in the registry.");
+                System.out.println("2. Add new animal.");
+                System.out.println("3. Add new command to animal.");
+                System.out.println("4. Show list of commands for animal.");
+                System.out.println("5. Exit.");
+                int choice = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (choice) {
+                    case 1: 
+                    case 2: 
+                    case 3: 
+                    case 4:
+                    case 5: System.out.println("Goodbye"); 
+                        return;
+                    default: System.out.println("Error: Incorrect choice");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Incorrect enter");
+				scanner.nextLine();
+            }
+        }
+    
+    }
+
+
 }
