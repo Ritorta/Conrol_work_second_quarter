fc0a9d4749876239aca29e89ba3adac46fc0d721
Ritorta <Meddesu@yandex.ru>
2024-02-23 15:43:46 +0500

    create abstract class home and pack animals

---
+++ Task/Task_1_to_12/data/pack_animals.java
@@ -0,0 +1,40 @@
+package Task.Task_1_to_12.data;
+
+import java.sql.Date;
+import java.util.List;
+
+public abstract class pack_animals extends animals {
+
+    private String name;
+    private String color;
+    private Date date_birth;
+    private List<String> commands;
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public Date getDate_birth() {
+        return date_birth;
+    }
+    public void setDate_birth(Date date_birth) {
+        this.date_birth = date_birth;
+    }
+    public List<String> getCommands() {
+        return commands;
+    }
+    public void setCommands(List<String> commands) {
+        this.commands = commands;
+    }
+
+    
+
+}
