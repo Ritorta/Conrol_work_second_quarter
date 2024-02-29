aa7b960722a29f94647b73ea513bc73492435956
Ritorta <Meddesu@yandex.ru>
2024-02-29 16:12:55 +0500

    almost filnale buld - update save, load and new comand metods

--- Task/Task_13_to_15/service/ServiceDatabase.java
+++ Task/Task_13_to_15/service/ServiceDatabase.java
@@ -32,165 +32,73 @@ public class ServiceDatabase {
 
     private final List<Animal> animals;
     private static final String filePath = "Task/Task_13_to_15/data/Database.txt";
-    
+	// Вызов загрузки базы данных
     public ServiceDatabase(){
         animals = new ArrayList<>();
         loadDatabase();      
     }
-
+	// Вызов сохранения базы данных
 	public void addAnimal(Animal animal){
         animals.add(animal);
-        saveDatabase(animal); // Old.
-		// saveDatabase(); // New.
+        // saveDatabase(animal); // Old.
+		saveDatabase(); // New.
     }
-
 	// Old.Сохранение в базу данных
-	private void saveDatabase(Animal anima) {
-		try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-			SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-			Set<Integer> Set = new HashSet<>();
+	// private void saveDatabase(Animal anima) {
+	// 	try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+	// 		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+	// 		Set<Integer> Set = new HashSet<>();
 
-			String header = "Type, ID, Name, Color, Date of Birth, Commands";
-        	writer.write(header);
-        	writer.newLine();
+	// 		String header = "Type, ID, Name, Color, Date of Birth, Commands";
+    //     	writer.write(header);
+    //     	writer.newLine();
 
-			for (Animal animal : animals) {
-				String className = animal.getClass().getSimpleName();
-				int id = animal.getId();
+	// 		for (Animal animal : animals) {
+	// 			String className = animal.getClass().getSimpleName();
+	// 			int id = animal.getId();
 
-				if (!Set.add(id)) {
-					System.out.println("Error: Duplicate ID " + id + " found. Skipping animal.");
-					continue;
-				}
+	// 			if (!Set.add(id)) {
+	// 				System.out.println("Error: Duplicate ID " + id + " found. Skipping animal.");
+	// 				continue;
+	// 			}
 	
-				if (animal instanceof HomeAnimals) {
-					HomeAnimals homeAnimal = (HomeAnimals) animal;
-					String line = className + ", " + id + ", " + homeAnimal.getName() + ", " + homeAnimal.getColor() + ", " + sdf.format(homeAnimal.getDate_birth()) + ", " +
-							String.join(", ", homeAnimal.getCommands());
-					writer.write(line);
-					writer.newLine();
-				} else if (animal instanceof PackAnimals) {
-					PackAnimals packAnimal = (PackAnimals) animal;
-					String line = className + ", " + id + ", " + packAnimal.getName() + ", " + packAnimal.getColor() + ", " + sdf.format(packAnimal.getDate_birth()) + ", " +
-							String.join(", ", packAnimal.getCommands());
-					writer.write(line);
-					writer.newLine();
-				} else {
-					System.out.println("Incorrect animal type: " + className);
-				}
-			}
-			System.out.println("Database save complete.");
-		} catch (IOException e) {
-			System.out.println("Error saving database: " + e.getMessage());
-		}
-	}
-	// Old.Загрузка из базы данных
-    private void loadDatabase() {
-		try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-			reader.readLine();
-	
-			String line;
-			while ((line = reader.readLine()) != null) {
-				 String[] data = line.split(", (?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
-				// String[] data = line.split(",(?=(?:[^\"]*\"[^\"]*\")([^\"]*$)|\s|;)"); // Ошибка с разделением не хочет воспринимать пробелы.
-
-				if (data.length >= 6) {
-					String className = data[0];
-					int id = Integer.parseInt(data[1]);
-					String name = data[2];
-					String color = data[3];
-					Date date_birth;
-					try {
-						date_birth = new SimpleDateFormat("dd.MM.yyyy").parse(data[4]);
-					} catch (ParseException e) {
-						System.out.println("Error parsing date: " + data[4]);
-						continue;
-					}
-					String commands = String.join(", ", Arrays.copyOfRange(data, 5, data.length));
-	
-					Animal animal;
-					switch (className) {
-						case "Dog" -> animal = new Dog(id, name, color, date_birth, commands);
-						case "Cat" -> animal = new Cat(id, name, color, date_birth, commands);
-						case "Hamster" -> animal = new Hamster(id, name, color, date_birth, commands);
-	
-						case "Camel" -> animal = new Camel(id, name, color, date_birth, commands);
-						case "Donkey" -> animal = new Donkey(id, name, color, date_birth, commands);
-						case "Horse" -> animal = new Horse(id, name, color, date_birth, commands);
-						default -> {
-							System.out.println("Unknown animal class: " + className);
-							continue;
-						}
-					}
-					animals.add(animal);
-				} else {
-					System.out.println("Invalid data in file: " + line);
-				}
-			}
-			System.out.println("Database load complete.");
-		} catch (IOException e) {
-			System.out.println("Error read database: " + e.getMessage());
-		}
-	}
-	/*  Новая версия метода saveDatabase,
-	Внесены следующие изменения:
-	 - Реализована красивая таблица
-	*/
-	// private void saveDatabase() {
-    // try (PrintWriter writer = new PrintWriter(new FileWriter(filePath))) {
-    //     SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-    //     Set<Integer> uniqueIds = new HashSet<>();
-
-    //     String header = String.format("%-15s %-5s %-15s %-10s %-15s %-20s", "Animal Type", "ID", "Name", "Color", "Date of Birth", "Commands");
-    //     writer.println(header);
-
-    //     for (Animal animal : animals) {
-    //         String className = animal.getClass().getSimpleName();
-    //         int id = animal.getId();
-
-    //         // Check if the ID is unique
-    //         if (!uniqueIds.add(id)) {
-    //             System.out.println("Error: Duplicate ID " + id + " found. Skipping animal.");
-    //             continue;
-    //         }
-
-    //         if (animal instanceof HomeAnimals) {
-    //             HomeAnimals homeAnimal = (HomeAnimals) animal;
-    //             String line = String.format("%-15s %-5d %-15s %-10s %-15s %-20s", className, id, homeAnimal.getName(), homeAnimal.getColor(), sdf.format(homeAnimal.getDate_birth()),
-    //                     String.join(", ", homeAnimal.getCommands()));
-    //             writer.println(line);
-    //         } else if (animal instanceof PackAnimals) {
-    //             PackAnimals packAnimal = (PackAnimals) animal;
-    //             String line = String.format("%-15s %-5d %-15s %-10s %-15s %-20s", className, id, packAnimal.getName(), packAnimal.getColor(), sdf.format(packAnimal.getDate_birth()),
-    //                     String.join(",", packAnimal.getCommands()));
-    //             writer.println(line);
-    //         } else {
-    //             System.out.println("Incorrect animal type: " + className);
-    //         }
-    //     }
-    //     System.out.println("Database save complete.");
+	// 			if (animal instanceof HomeAnimals) {
+	// 				HomeAnimals homeAnimal = (HomeAnimals) animal;
+	// 				String line = className + ", " + id + ", " + homeAnimal.getName() + ", " + homeAnimal.getColor() + ", " + sdf.format(homeAnimal.getDate_birth()) + ", " +
+	// 						String.join(", ", homeAnimal.getCommands());
+	// 				writer.write(line);
+	// 				writer.newLine();
+	// 			} else if (animal instanceof PackAnimals) {
+	// 				PackAnimals packAnimal = (PackAnimals) animal;
+	// 				String line = className + ", " + id + ", " + packAnimal.getName() + ", " + packAnimal.getColor() + ", " + sdf.format(packAnimal.getDate_birth()) + ", " +
+	// 						String.join(", ", packAnimal.getCommands());
+	// 				writer.write(line);
+	// 				writer.newLine();
+	// 			} else {
+	// 				System.out.println("Incorrect animal type: " + className);
+	// 			}
+	// 		}
+	// 		System.out.println("Database save complete.");
 	// 	} catch (IOException e) {
 	// 		System.out.println("Error saving database: " + e.getMessage());
 	// 	}
 	// }
-	/*  Новая версия метода loadDatabase,
-	Внесены следующие изменения:
-	 - Чтение с пробелами, вроде работает.
-	*/
-	// private void loadDatabase() {
-	// 	try (Scanner scanner = new Scanner(new File(filePath))) {
-	// 		animals.clear();
-	// 		scanner.nextLine();
+	// Old.Загрузка из базы данных
+    // private void loadDatabase() {
+	// 	try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+	// 		reader.readLine();
+	
+	// 		String line;
+	// 		while ((line = reader.readLine()) != null) {
+	// 			 String[] data = line.split(", (?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+	// 			// String[] data = line.split(",(?=(?:[^\"]*\"[^\"]*\")([^\"]*$)|\s|;)"); // Ошибка с разделением не хочет воспринимать пробелы.
 
-	// 		while (scanner.hasNextLine()) {
-	// 			String line = scanner.nextLine();
-	// 			String[] data = line.split("\\s+");
-		
-	// 			String className = data[0];
-	// 			int id = Integer.parseInt(data[1]);
-	// 			String name = data[2];
-	// 			String color = data[3];
-	// 			Date date_birth;
+	// 			if (data.length >= 6) {
+	// 				String className = data[0];
+	// 				int id = Integer.parseInt(data[1]);
+	// 				String name = data[2];
+	// 				String color = data[3];
+	// 				Date date_birth;
 	// 				try {
 	// 					date_birth = new SimpleDateFormat("dd.MM.yyyy").parse(data[4]);
 	// 				} catch (ParseException e) {
@@ -198,8 +106,8 @@ public class ServiceDatabase {
 	// 					continue;
 	// 				}
 	// 				String commands = String.join(", ", Arrays.copyOfRange(data, 5, data.length));
-		
-	// 			Animal animal;
+	
+	// 				Animal animal;
 	// 				switch (className) {
 	// 					case "Dog" -> animal = new Dog(id, name, color, date_birth, commands);
 	// 					case "Cat" -> animal = new Cat(id, name, color, date_birth, commands);
@@ -213,36 +121,124 @@ public class ServiceDatabase {
 	// 						continue;
 	// 					}
 	// 				}
-	// 			animals.add(animal);
+	// 				animals.add(animal);
+	// 			} else {
+	// 				System.out.println("Invalid data in file: " + line);
+	// 			}
 	// 		}
 	// 		System.out.println("Database load complete.");
-	// 	} catch (FileNotFoundException e) {
-	// 		System.out.println("Database file not found.");
-		
+	// 	} catch (IOException e) {
+	// 		System.out.println("Error read database: " + e.getMessage());
 	// 	}
 	// }
+	/*  Новая версия метода saveDatabase,
+	Внесены следующие изменения:
+	 - Реализована красивая таблица
+	*/
+	private void saveDatabase() {
+    try (PrintWriter writer = new PrintWriter(new FileWriter(filePath))) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        Set<Integer> uniqueIds = new HashSet<>();
+
+        String header = String.format("%-15s %-5s %-18s %-20s %-15s %-20s", "Animal Type", "ID", "Name", "Color", "Date of Birth", "Commands");
+        writer.println(header);
+
+        for (Animal animal : animals) {
+            String className = animal.getClass().getSimpleName();
+            int id = animal.getId();
+
+            if (!uniqueIds.add(id)) {
+                System.out.println("Error: Duplicate ID " + id + " found. Skipping animal.");
+                continue;
+            }
+
+            if (animal instanceof HomeAnimals) {
+                HomeAnimals homeAnimal = (HomeAnimals) animal;
+                String line = String.format("%-15s %-5d %-18s %-20s %-15s %-20s", className, id, homeAnimal.getName(), homeAnimal.getColor(), sdf.format(homeAnimal.getDate_birth()),
+                        String.join(" ", homeAnimal.getCommands()));
+                writer.println(line);
+            } else if (animal instanceof PackAnimals) {
+                PackAnimals packAnimal = (PackAnimals) animal;
+                String line = String.format("%-15s %-5d %-18s %-20s %-15s %-20s", className, id, packAnimal.getName(), packAnimal.getColor(), sdf.format(packAnimal.getDate_birth()),
+                        String.join(" ", packAnimal.getCommands()));
+                writer.println(line);
+            } else {
+                System.out.println("Incorrect animal type: " + className);
+            }
+        }
+        System.out.println("Database save complete.");
+		} catch (IOException e) {
+			System.out.println("Error saving database: " + e.getMessage());
+		}
+	}
+	/*  Новая версия метода loadDatabase,
+	Внесены следующие изменения:
+	 - Чтение с пробелами
+	*/
+	private void loadDatabase() {
+		try (Scanner scanner = new Scanner(new File(filePath))) {
+			animals.clear();
+			scanner.nextLine();
+
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				String[] data = line.split("\\s+");
+		
+				String className = data[0];
+				int id = Integer.parseInt(data[1]);
+				String name = data[2];
+				String color = data[3];
+				Date date_birth;
+					try {
+						date_birth = new SimpleDateFormat("dd.MM.yyyy").parse(data[4]);
+					} catch (ParseException e) {
+						System.out.println("Error parsing date: " + data[4]);
+						continue;
+					}
+					String commands = String.join(" ", Arrays.copyOfRange(data, 5, data.length));
+		
+				Animal animal;
+					switch (className) {
+						case "Dog" -> animal = new Dog(id, name, color, date_birth, commands);
+						case "Cat" -> animal = new Cat(id, name, color, date_birth, commands);
+						case "Hamster" -> animal = new Hamster(id, name, color, date_birth, commands);
+	
+						case "Camel" -> animal = new Camel(id, name, color, date_birth, commands);
+						case "Donkey" -> animal = new Donkey(id, name, color, date_birth, commands);
+						case "Horse" -> animal = new Horse(id, name, color, date_birth, commands);
+						default -> {
+							System.out.println("Unknown animal class: " + className);
+							continue;
+						}
+					}
+				animals.add(animal);
+			}
+			System.out.println("Database load complete.");
+		} catch (FileNotFoundException e) {
+			System.out.println("Database file not found.");
+		
+		}
+	}
 	// Old.Добавление новой команды для животного по id
 	// public void NewCommand(int id, String command) {
 	// 	for (Animal animal : animals) {
 	// 		if (animal.getId() == id) {
-	// 			String[] commands = command.split(",");
+	// 			String[] commands = command.split(", ");
 	// 			for (int i = 0; i < commands.length; i++) {
 	// 				String trimmedCommand = commands[i].trim();
 	// 				commands[i] = trimmedCommand;
 	// 			}
-	// 			animal.NewCommand(command);
-	// 			saveDatabase(); // Повторное добавление плодит пробелы и приводит к ошибке, если испольховать разделение по пробелам
+	// 			animal.NewCommand(" " + String.join(", ", commands));
+	// 			saveDatabase(animal);
 	// 			System.out.println("Commands add sucess.");
 	// 			return;
 	// 		}
 	// 	}
 	// 	System.out.println("Animals id " + id + " not found.");
 	// }
-	
 	/* Новая версия метода NewCommand,
 	- Сохранение без дублирования пробелов.
 	- Добавляет пробел после запятой в самом начале.
-	С новыми методами плодит запятые...
 	*/
 	public void NewCommand(int id, String command) {
 		for (Animal animal : animals) {
@@ -253,8 +249,7 @@ public class ServiceDatabase {
 					commands[i] = trimmedCommand;
 				}
 				animal.NewCommand(" " + String.join(", ", commands));
-				// saveDatabase(); // New.
-				saveDatabase(animal); // Old.
+				saveDatabase(); // New.
 				System.out.println("Commands add sucess.");
 				return;
 			}
